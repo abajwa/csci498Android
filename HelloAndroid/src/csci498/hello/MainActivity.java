c0cc0991f5340bd1abfb7f8777b8babd1cbee1c5
Amanreet Bajwa <abajwa@mines.edu>
2012-08-23 20:33:00 -0600

    Added a onClickListener that changes the main screen text to the current date

--- HelloAndroid/src/csci498/hello/MainActivity.java
+++ HelloAndroid/src/csci498/hello/MainActivity.java
@@ -1,20 +1,29 @@
 package csci498.hello;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+	Button btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        btn = new Button(this);
+        btn.setOnClickListener(this);
+        updateTime();
+        setContentView(btn);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+    public void onClick(View view) {
+    	updateTime();
+    }
+    
+    private void updateTime() {
+    	btn.setText(new Date().toString());
     }
 }
