3f9869dbc5a6e4ac1755643804ca551a0082e3fc
Amanreet Bajwa <abajwa@mines.edu>
2012-09-28 23:30:03 -0600

    modified the DetailForm class to work with the new feed

--- LunchList/src/csci498/abajwa/lunchlist/DetailForm.java
+++ LunchList/src/csci498/abajwa/lunchlist/DetailForm.java
@@ -10,8 +10,9 @@ import android.widget.RadioGroup;
 
 public class DetailForm extends Activity {
 	
-	EditText name;
 	EditText address;
+	EditText feed;
+	EditText name;
 	EditText notes;
 	RadioGroup types;
 	
@@ -28,6 +29,7 @@ public class DetailForm extends Activity {
 		address = (EditText)findViewById(R.id.addr);
 		types = (RadioGroup)findViewById(R.id.types);
 		notes = (EditText)findViewById(R.id.notes);
+		feed = (EditText)findViewById(R.id.feed);
 
 		Button save = (Button)findViewById(R.id.save);
 
@@ -75,6 +77,7 @@ public class DetailForm extends Activity {
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		
 		if (helper.getType(c).equals("sit_down")) {
 			types.check(R.id.sit_down);
@@ -108,10 +111,10 @@ public class DetailForm extends Activity {
 			}
 			
 			if (restaurantId == null) {
-				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
 			}
 			else {
-				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
 			}
 			
 			finish();
