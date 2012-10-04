f7631f31f87f4912ac0f828e9e536d5dab2f5bc5
Amanreet Bajwa <abajwa@mines.edu>
2012-10-04 08:39:59 -0600

    added the FeedService class and added it to the manifest

---
+++ LunchList/src/csci498/abajwa/lunchlist/FeedService.java
@@ -0,0 +1,17 @@
+package csci498.abajwa.lunchlist;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class FeedService extends IntentService {
+	
+	public FeedService() {
+		super("FeedService");
+	}
+	
+	@Override
+	public void onHandleIntent(Intent i) {
+		// do something
+	}
+	
+}
