74bed6e512a74df0e31702077876b521a0924d7c
Amanreet Bajwa <abajwa@mines.edu>
2012-10-04 08:43:11 -0600

    implemented onHandleIntent in FeedService

--- LunchList/src/csci498/abajwa/lunchlist/FeedService.java
+++ LunchList/src/csci498/abajwa/lunchlist/FeedService.java
@@ -1,9 +1,15 @@
 package csci498.abajwa.lunchlist;
 
+import org.mcsoxford.rss.RSSFeed;
+import org.mcsoxford.rss.RSSReader;
+
 import android.app.IntentService;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedService extends IntentService {
+	
+	public static final String EXTRA_URL = "apt.tutorial.EXTRA_URL";
 	
 	public FeedService() {
 		super("FeedService");
@@ -11,7 +17,14 @@ public class FeedService extends IntentService {
 	
 	@Override
 	public void onHandleIntent(Intent i) {
-		// do something
+		RSSReader reader = new RSSReader();
+		
+		try {
+			RSSFeed result = reader.load(i.getStringExtra(EXTRA_URL));
+		}
+		catch (Exception e) {
+			Log.e("LunchList", "Exception parsing feed", e);
+		}
 	}
 	
 }
