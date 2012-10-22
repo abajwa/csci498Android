e637498a4f6f55aa62d9ae244e2d5053107c1f35
Amanreet Bajwa <abajwa@mines.edu>
2012-10-21 20:56:39 -0600

    Added the latitude and longitude to the detail form and restaurant map

--- LunchList/src/csci498/abajwa/lunchlist/DetailForm.java
+++ LunchList/src/csci498/abajwa/lunchlist/DetailForm.java
@@ -30,6 +30,9 @@ public class DetailForm extends Activity {
 	String restaurantId;
 	LocationManager locMgr;
 	
+	double latitude;
+	double longitude;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -84,6 +87,11 @@ public class DetailForm extends Activity {
 		}
 		else if (item.getItemId() == R.id.map) {
 			Intent i = new Intent(this, RestaurantMap.class);
+			
+			i.putExtra(RestaurantMap.EXTRA_LATITUDE, latitude);
+			i.putExtra(RestaurantMap.EXTRA_LONGITUDE, longitude);
+			i.putExtra(RestaurantMap.EXTRA_NAME, name.getText().toString());
+			
 			startActivity(i);
 			
 			return true;
@@ -153,6 +161,9 @@ public class DetailForm extends Activity {
 		else {
 			types.check(R.id.delivery);
 		}
+		
+		latitude = helper.getLatitude(c);
+		longitude = helper.getLongitude(c);
 		
 		location.setText(String.valueOf(helper.getLatitude(c)) + ", " + String.valueOf(helper.getLongitude(c)));
 		
